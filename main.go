3cd94db5b6aaa822741bc9f4c07cb569f54febd2
Akira Chiku <akira.chiku@gmail.com>
2017-03-06 00:05:25 +0900

    Update

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 
+	toggl "github.com/achiku/go-toggl"
 	"github.com/gizak/termui"
 )
 
@@ -13,24 +17,32 @@ func main() {
 	}
 	defer termui.Close()
 
-	//termui.UseTheme("helloworld")
+	src, err := os.Open("./testdata/response_projects.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var prjs []toggl.Project
+	decoder := json.NewDecoder(src)
+	if err := decoder.Decode(&prjs); err != nil {
+		log.Fatal(err)
+	}
 
 	bc := termui.NewMBarChart()
 	math := []int{90, 85, 90, 80}
 	english := []int{70, 85, 75, 60}
 	science := []int{75, 60, 80, 85}
 	compsci := []int{100, 100, 100, 100}
+	bc.BorderLabel = "aa"
 	bc.Data[0] = math
 	bc.Data[1] = english
 	bc.Data[2] = science
 	bc.Data[3] = compsci
 	studentsName := []string{"12", "13", "14", "15"}
 	bc.ShowScale = true
-	bc.BorderLabel = "Student's Marks X-Axis=Name Y-Axis=Marks[Math,English,Science,ComputerScience] in %"
+	bc.BorderLabel = "scores"
 	bc.BarWidth = 20
 	bc.Width = 50
 	bc.Height = 30
-	bc.Y = 3
 	bc.BarWidth = 10
 	bc.DataLabels = studentsName
 	bc.ShowScale = true //Show y_axis scale value (min and max)
@@ -60,7 +72,6 @@ func main() {
 	bclabels := []string{"S0", "S1", "S2", "S3", "S4", "S5"}
 	bc1.BorderLabel = "Bar Chart"
 	bc1.Data = data
-	bc1.Y = 33
 	bc1.Width = 26
 	bc1.Height = 10
 	bc1.DataLabels = bclabels
@@ -68,10 +79,37 @@ func main() {
 	bc1.BarColor = termui.ColorRed
 	bc1.NumColor = termui.ColorYellow
 
-	termui.Render(bc, bc1, g0)
+	var strs []string
+	for i, prj := range prjs {
+		strs = append(strs, fmt.Sprintf("[%d] %s", i, prj.Name))
+	}
+	ls := termui.NewList()
+	ls.Items = strs
+	ls.ItemFgColor = termui.ColorYellow
+	ls.BorderLabel = "List"
+	ls.Height = 10
+
+	termui.Body.AddRows(
+		termui.NewRow(
+			termui.NewCol(12, 0, g0),
+		),
+		termui.NewRow(
+			termui.NewCol(4, 0, bc),
+			termui.NewCol(4, 0, bc1),
+			termui.NewCol(4, 0, ls),
+		),
+	)
+
+	// calculate layout
+	termui.Body.Align()
+
+	termui.Render(bc, bc1, g0, ls)
 
 	termui.Handle("/sys/kbd/q", func(termui.Event) {
 		termui.StopLoop()
+	})
+	termui.Handle("/sys/kbd/r", func(termui.Event) {
+		termui.Render(bc, bc1, g0, ls)
 	})
 	termui.Loop()
 }
